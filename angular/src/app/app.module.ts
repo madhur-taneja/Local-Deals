9a0ad8de5c1c38076b094d8c88d61624dd324137
Ashutosh Sharma <mainhuashutosh@gmail.com>
2018-05-05 22:47:53 +0530

    Pulling login page to master

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,8 +6,19 @@ import { AppComponent } from './app.component';
 import { CategoriesPageComponent } from './categories-page/categories-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { CategoriesPageContentComponent } from './categories-page-content/categories-page-content.component';
-
+import { RouterModule, Routes } from '@angular/router';
 import { ShopkeeperService } from './services/shopkeeper.service';
+
+// To implement routing facility so that we can route components on basics of URL.
+// Array of routes in the whole website.
+const routes: Routes = [
+  // redirect to home when user types URL as localhost:4200
+  { path: '', redirectTo:'home', pathMatch:'full' },
+  { path: 'login', component : MainPageComponent },
+  { path: 'home', component : MainPageComponent },
+  { path: 'shopkeeper/dashboard', component : CategoriesPageComponent }
+]
+
 
 @NgModule({
   declarations: [
